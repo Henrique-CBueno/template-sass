70bc4730c62560c5651249c11cc65a67a6053ab5
Henrique-CBueno <henriquecb2507@outlook.pt>
2025-04-18 13:43:09 -0300

    feat: implement NextAuth with Google provider and Firestore adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,19 @@
-import { handlers } from "@/app/lib/auth";
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import firestore from "../../../lib/firestore";
 
-export const authOptions = {
+const handler = NextAuth({
   adapter: FirestoreAdapter(firestore),
-  // providers, callbacks, etc
-};
- 
- export const { GET, POST } = handlers;
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  // callbacks, pages, etc (se precisar)
+});
+
+export { handler as GET, handler as POST };
